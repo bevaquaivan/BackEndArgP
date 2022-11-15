c824ab59ee31801b68f13fad1084315d9fcf44d1
Iván <bevaquaivan@gmail.com>
2022-11-15 11:23:23 -0300

    intento de debug

--- src/main/java/com/portfolio/mgb/Controller/CEducacion.java
+++ src/main/java/com/portfolio/mgb/Controller/CEducacion.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//probando
 package com.portfolio.mgb.Controller;
 
 import com.portfolio.mgb.Dto.dtoEducacion;
