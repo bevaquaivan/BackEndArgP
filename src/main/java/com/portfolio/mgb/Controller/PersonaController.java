ca643554e25da69358e42daa9c925b4450d58431
Iván <bevaquaivan@gmail.com>
2022-11-15 11:02:46 -0300

    intento de debug

--- src/main/java/com/portfolio/mgb/Controller/PersonaController.java
+++ src/main/java/com/portfolio/mgb/Controller/PersonaController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "https://argprograma-portfolio-front.web.app/")
+@CrossOrigin(origins = "https://argprograma-portfolio-front.web.app")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
@@ -62,3 +62,4 @@ public class PersonaController {
     }
    
 }
+
